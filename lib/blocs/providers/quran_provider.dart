43242e714649f2e450988ad80df80556e00fe362
Mohamed Ayad <mohamed.ayad@kortobaa.com>
2022-12-02 11:02:56 +0200

    refactor quran api

---
+++ lib/blocs/providers/quran_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:great_quran/blocs/models/quran/surah.dart';
+import 'package:great_quran/blocs/states/generic_state.dart';
+import 'package:great_quran/data/remote/apis/quran_api.dart';
+import 'package:great_quran/data/remote/interfaces/i_quran_api.dart';
+
+class QuranNotifier extends StateNotifier<GenericState<List<Surah>>> {
+  static final provider = StateNotifierProvider((ref) {
+    return QuranNotifier(ref.read(QuranApi.provider));
+  });
+  final IQuranApi _iQuranApi;
+  QuranNotifier(this._iQuranApi) : super(GenericState.initial());
+
+  Future<void> getQuran() async {
+    try {
+      state = GenericState.loading();
+      final data = await _iQuranApi.getQuran();
+      state = GenericState.success(data);
+    } catch (e) {
+      state = GenericState.fail();
+      debugPrint('Error $e');
+    }
+  }
+}
