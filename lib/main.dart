81d6fdb3e11eb640f637329c5a10f8274c531f1a
Mohamed Ayad <mohamed.ayad@kortobaa.com>
2022-12-09 09:50:16 +0200

    add status Bar Color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:great_quran/ui/app.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle.dark.copyWith(
+      statusBarColor: Colors.grey,
+    ),
+  );
+  runApp(MyApp());
+}
